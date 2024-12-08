381ac369af20d2df029d4e70909b196ac536b1ef
angie <angelica.ekman@hotmail.com>
2024-12-08 22:32:52 +0100

    addedthings

--- app/src/main/java/com/example/higherlowercardgame/GameActivity.kt
+++ app/src/main/java/com/example/higherlowercardgame/GameActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
--- app/src/main/java/com/example/higherlowercardgame/MainActivity.kt
+++ app/src/main/java/com/example/higherlowercardgame/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.higherlowercardgame
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
 
